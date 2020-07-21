a755c3b55de233babce943c5d0e46e04b82b1450
Osama <osamafahim997@gmail.com>
2020-07-21 13:03:06 +0500

    Initial Commit

---
+++ AutoLayout Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayout Calculator
+//
+//  Created by Osama Fahim on 15/06/2019.
+//  Copyright © 2019 Osama Fahim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
